f9bc1b23bbdb6cc51b0c47d619ff48696981d723
Alex Choi <alex.ji.choi@gmail.com>
2020-10-03 14:41:15 -0400

    initial commit for test

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AlertController } from '@ionic/angular';
+import {
+	Plugins,
+	LocalNotificationEnabledResult,
+	LocalNotificationActionPerformed,
+	LocalNotification,
+	Device
+} from '@capacitor/core';
+const { LocalNotifications } = Plugins;
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+	selector: 'app-home',
+	templateUrl: 'home.page.html',
+	styleUrls: [ 'home.page.scss' ]
 })
-export class HomePage {
+export class HomePage implements OnInit {
+	constructor(private alertCtrl: AlertController) {}
 
-  constructor() {}
+	async ngOnInit() {
+		await LocalNotifications.requestPermission();
+	}
 
+	async scheduleBasic() {
+		var hi = 'asdfsadf';
+		LocalNotifications.schedule({
+			notifications: [
+				{
+					title: 'Friendly Reminder',
+					body: 'Join the Army',
+					id: 1,
+					extra: {
+						data: 'Pass data to your handler'
+					},
+					iconColor: '#0000FF'
+				}
+			]
+		});
+	}
+
+	async scheduleAdvanced() {}
+
+	async presentAlert(header, message) {
+		const alert = await this.alertCtrl.create({
+			header,
+			message,
+			buttons: [ 'OK' ]
+		});
+
+		await alert.present();
+	}
 }
